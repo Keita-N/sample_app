c8adc850499df8aedbdf225a99bdaad5bc390ae4
Keita-N <keita.nagatomi@gmail.com>
2014-05-11 15:50:06 +0900

    Finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -6,8 +6,29 @@ def valid_signin(user)
   click_button "Sign in"
 end
 
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Capybaraを使用していない場合にもサインインする。
+  cookies[:remember_token] = user.remember_token
+end
+
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
     page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-success', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_title do |title|
+  match do |page|
+    page.should have_selector('title', text:title)
   end
 end
